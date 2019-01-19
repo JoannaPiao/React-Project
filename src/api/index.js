1000bc5f9000b32f05550da866721cc467fc7355
xuyichen <757502262@qq.com>
2019-01-19 08:46:21 +0800

    异步显示分类列表

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,7 @@
  */
 
 import ajax from './ajax'
+import jsonp from 'jsonp'
 
 //登录
 //传入对象 名称要对应  位置可以不用对应 但是用参数传  位置必须对应上   如果传的是参数 接收也要用参数  传的是对象 接收也要用对象
@@ -27,4 +28,34 @@ export function reqLogin({username, password}) {
 
 export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST')
 
+
+
+//请求获取天气
+export const reqWeather = (city) => {
+    return new Promise(function (resolve, reject) {
+        //发送异步ajax请求
+        const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+        jsonp(url, {
+                param: 'callback'
+            }, (error, data) => {
+                console.log('callback', error, data);
+                if (!error) {//如果成功了 ，调用resolve传递数据
+                    const {dayPictureUrl, weather} = data.results[0].weather_data[0]
+                    resolve({dayPictureUrl, weather})
+
+                } else { //如果失败了，返回error
+                    alert('请求接口出错了')
+                }
+            }
+        )
+    })
+}
+
+
+//获取一级/二级分类列表
+export const reqCategorys = (parentId) => ajax('manage/category/list',{parentId})
+//添加分类
+export const reqAddCategorys = (parentId,categoryName) => ajax('manage/category/add',{parentId,categoryName},'POST')
+//更新分类名称
+export const reqUpdateCategorys = (categoryId ,categoryName) => ajax('manage/category/update',{categoryId ,categoryName},'POST')
 
--- src/pages/category/category.jsx
+++ src/pages/category/category.jsx
@@ -1,14 +1,83 @@
 import React, {Component} from 'react'
+import {
+    Card,
+    Table,
+    Button,
+    Icon
+} from 'antd'
 
+import {reqCategorys} from '../../api'
 
 /*
 管理的分类管理路由组件
  */
 export default class Category extends Component {
+    state = {
+        category: []
+    }
+    /*
+    获取一级分类列表
+     */
+    getCategorys = async () => {
+        const result = await reqCategorys('0')
+        if (result.status === 0) {
+            const categorys = result.data
+            //更新状态
+            this.setState({
+                category:categorys
+            })
+        }
+    }
+
+    componentDidMount() {
+        this.getCategorys()
+    }
+
+    componentWillMount() {
+        this.columns = [{
+            title: '分类名称',
+            dataIndex: 'name',
+            /*/
+            render代码是变成链接
+             */
+            // render: text => <a href="javascript:;">{text}</a>,
+        }, {
+            title: '操作',
+            width: 300,
+            render: (category) => {
+                return (
+                    <span>
+                        <a href='javascript:'>修改分类</a>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <a href='javascript:'>查看子分类</a>
+                    </span>
+                )
+            }
+        }];
+    }
+
     render() {
+        //得到列的分组
+        const columns = this.columns
+        //得到分类的数组
+        const {category}= this.state
         return (
-            <div >
-                Category
+            <div>
+                <Card>
+                    <span style={{fontSize: 22}}>一级分类列表</span>
+                    <Button type='primary' style={{float: 'right'}}>
+                        <Icon type='plus'></Icon>
+                        添加分类
+                    </Button>
+                </Card>
+                <Table
+                    rowKey='_id'
+                    bordered
+                    columns={columns}
+                    dataSource={category}
+                    loading={!category|| category.length === 0}
+                    pagination={{defaultPageSize: 10, showSizeChanger: true, showQuickJumper: true}}
+                />
             </div>
         )
     }
